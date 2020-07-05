96d59e62566c925c1ab687f90f44aba37f7763ca
Gregório Patriota <gpc.cin@gmail.com>
2020-07-04 22:13:38 -0300

    WIP: creating and setting up the docker-compose file  - All services is okay, but the service 01 and service 02 does not find the rabbitmq service

---
+++ services/order_service/main/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response
+from json import dumps
+from flask_nameko import FlaskPooledClusterRpcProxy
+
+app = Flask(__name__)
+rpc = FlaskPooledClusterRpcProxy()
+app.config.update(dict(
+        NAMEKO_AMQP_URI='amqp://guest:guest@localhost:5672'
+    ))
+
+rpc.init_app(app)
+
+
+@app.route('/')
+def hello_world():
+    return Response(response=dumps({'msg': 'Hello World'}),
+                    content_type='Application/json',
+                    status=200)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
